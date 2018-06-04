59f8aff3c468c212023ec3fac5508e5d0d419bf0
Christian <christiangoodridge@gmail.com>
2018-06-03 22:31:55 -0230

    -Adding rules now works -Rulelist is now populated based off an array of rules -The Rule View modal will now properly pull rule data -Minor Tweaks

--- client/scripts/Rule.js
+++ client/scripts/Rule.js
@@ -11,4 +11,50 @@ Rule = function(name, description, value)
         {
             player.score(self.value)
         }
+        return self;
+};
+
+populateRuleList = function()
+{
+    var rules = global.rules;
+    var ruleNumber = rules.length;
+    var html = "";
+
+    for(i = 0; i < rules.length; i++)
+    {
+        html += "<button type = button class = \"list-group-item list-group-item-action\" data-toggle=\"modal\" data-target=\"#ruleViewModal\" data-rNo = '"+i+"' + onClick='getRule(this.getAttribute(\"data-rNo\"))'>" +
+            rules[i].name +"</button>\n"
+    }
+
+    $("#RuleScroll").html(html);
+};
+
+newRule = function()
+{
+    var name = $("#rlName").val();
+    var disc = $("#rlDisc").val();
+    var value = $("#rlVal").val();
+    global.rules.push(Rule(name, disc, value));
+    $("#newRuleModal").modal('hide');
+    populateRuleList();
+    $("#rlName").val("");
+    $("#rlDisc").val("");
+    $("#rlVal").val("");
+};
+
+getRule = function(rNo)
+{
+    var rule = global.rules[rNo];
+    $("#ruleViewModal").attr("data-rNo",rNo);
+    $("#RuleTitle").html(rule.name);
+    $("#RuleDisc").html(rule.description);
+    $("#RuleVal").html(rule.value);
+
+};
+
+removeRule = function(rNo)
+{
+ global.rules.splice(rNo,1);
+ $("#ruleViewModal").modal('hide');
+ populateRuleList();
 }
